5f622fdf3d268edeca8b38f2cf5aaf36f6ae66a2
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-11-09 16:03:00 -0300

    cors

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const nodemailer = require("nodemailer");
 const express = require('express');
 const app = express();
 const routes = require('./routes/app.routes');
+var cors = require('cors');
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json()); //tornar o corpo inteligivel para o javascript. Transforma a requisição em json
 app.use(routes);
-app.listen(process.env.PORT || 4000, function(){
+app.listen(process.env.
+    PORT || 4000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
